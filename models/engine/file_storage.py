b2167f6e4f6a32778a1192e575d6c3aa1f10cbf5
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-08-08 07:33:27 +0200

    5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,13 +6,14 @@ import models
 
 
 class FileStorage:
-      """Represent an abstracted storage engine.
+      """
+      Represent an abstracted storage engine.
 
       Attributes:
              __file_path (str): The name of the file to save objects to.
              __objects (dict): A dictionary of instantiated objects.
       """
-
+      
     __file_path = "file.json"
     __objects = {}
 
