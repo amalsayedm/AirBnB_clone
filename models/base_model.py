4ea68ed2d09c3b6fb4fdc42b51f5a2668fb63731
amalsayedm <amal.sayedm2@gmail.com>
2023-08-08 14:36:10 +0200

    amal

--- models/base_model.py
+++ models/base_model.py
@@ -25,16 +25,19 @@ class BaseModel:
                     self.__dict__["updated_at"] = datetime.strptime(
                         kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 else:
-                    self.id = str(uuid.uuid4())
+                    self.id = str(uuid4())
                     self.created_at = datetime.now()
                     self.updated_at = datetime.now()
-                    storage.new(self)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            
 
     def save(self):
         """Update updated_at with the current datetime."""
 
         self.updated_at = datetime.today()
-        models.storage.save()
 
     def __str__(self):
         """Returns official string representation"""
@@ -52,4 +55,4 @@ class BaseModel:
         my_dct['created_at'] = self.created_at.strftime(
             "%Y-%m-%dT%H:%M:%S.%f")
 
-        return (cp_dct)
+        return (my_dct)
