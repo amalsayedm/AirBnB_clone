de2b88b20b79859868eb4099c9ae0bb87635f34b
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-08-08 14:30:31 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -33,7 +33,7 @@ class BaseModel:
     def save(self):
         """Update updated_at with the current datetime."""
 
-        self.updated_at = datetime.today()
+        self.updated_at = datetime.now()
         models.storage.save()
 
     def __str__(self):
