a7f5516464b56bb35391f8c2219b3cae74b88e1c
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-08-10 11:43:33 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -17,42 +17,38 @@ class BaseModel:
              **kwargs: dict of key-values arguments
         """
 
-        if kwargs is not None and kwargs != {}:
-            for key in kwargs:
-                if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
-                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(
-                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "id":
-                    self.id = kwargs["id"]
-
-        else:
+        if not kwargs:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             storage.new(self)
+        else:
+            kwargs["updated_at"] = datetime.strptime(
+                kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            kwargs["created_at"] = datetime.strptime(
+                kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            del kwargs['__class__']
+            self.__dict__.update(kwargs)
 
+    def __str__(self):
+        """Returns official string representation"""
+        cself = (str(type(self)).split('.')[-1]).split('\'')[0]
+        return "[{}] ({}) {}".format(cself, self.id, self.__dict__)
+        
     def save(self):
         """Update updated_at with the current datetime."""
         self.updated_at = datetime.now()
         storage.save()
 
-    def __str__(self):
-        """Returns official string representation"""
-
-        return "[{}] ({}) {}".\
-               format(type(self).__name__, self.id, self.__dict__)
-
     def to_dict(self):
         """Return dictionary representation of BaseModel class."""
-
-        my_dct = dict(self.__dict__)
-        my_dct['__class__'] = self.__class__.__name__
+        my_dct = {}
+        my_dct.update(self.__dict__)
+        my_dct.update({'__class__' :
+                      (str(type(self)).split('.')[-1]).split('\'')[0]})
         my_dct['updated_at'] = self.updated_at.strftime(
             "%Y-%m-%dT%H:%M:%S.%f")
         my_dct['created_at'] = self.created_at.strftime(
             "%Y-%m-%dT%H:%M:%S.%f")
 
-        return (my_dct)
+        return my_dct
