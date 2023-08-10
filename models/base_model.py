8c4431a9f6f78c443423548bb5f582ec1aece5c6
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-08-10 08:23:42 +0200

    styling

--- models/base_model.py
+++ models/base_model.py
@@ -32,8 +32,7 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
-            
+            storage.new(self)            
             
 
     def save(self):
