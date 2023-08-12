f2a2c2e7b9ced8d572f3e3a47b8decab852d4d68
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-08-12 02:02:21 +0200

    testing

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
-""" """
+"""state testing """
 from tests.test_models.test_base_model import test_basemodel
 from models.state import State
 
 
 class test_state(test_basemodel):
-    """ """
+    """state class testing """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """Initializes instance attributes
+
+         Args:
+            *args: list of arguments
+             **kwargs: dict of key-values arguments
+        """
         super().__init__(*args, **kwargs)
         self.name = "State"
         self.value = State
 
-    def test_name3(self):
-        """ """
+    def test_name(self):
+        """testing  name atribute (string) """
         new = self.value()
         self.assertEqual(type(new.name), str)
