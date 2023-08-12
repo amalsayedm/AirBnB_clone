3e52e6e1b356a7153f5556e5ce05eb6b7e4eab67
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-08-12 02:43:44 +0200

    testing

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" """
+"""testing user"""
 from tests.test_models.test_base_model import test_basemodel
 from models.user import User
 
 
 class test_User(test_basemodel):
-    """ """
+    """ class teting user """
 
     def __init__(self, *args, **kwargs):
         """ """
@@ -13,22 +13,23 @@ class test_User(test_basemodel):
         self.name = "User"
         self.value = User
 
+    """attributes testing """
     def test_first_name(self):
-        """ """
+        """first name -> string """
         new = self.value()
         self.assertEqual(type(new.first_name), str)
 
     def test_last_name(self):
-        """ """
+        """last name -> string """
         new = self.value()
         self.assertEqual(type(new.last_name), str)
 
     def test_email(self):
-        """ """
+        """E mail -> string """
         new = self.value()
         self.assertEqual(type(new.email), str)
 
     def test_password(self):
-        """ """
+        """ password -> string """
         new = self.value()
         self.assertEqual(type(new.password), str)
