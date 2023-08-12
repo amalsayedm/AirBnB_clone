4fdd1bd2f8e8cc2c4ec6d2c23584995119e8dbe1
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-08-12 02:05:35 +0200

    testing

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
-""" """
+"""amenity testing """
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
 
 
 class test_Amenity(test_basemodel):
-    """ """
+    """ amenity class testing"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """Initializes instance attributes
+
+         Args:
+            *args: list of arguments
+             **kwargs: dict of key-values arguments
+        """
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
 
-    def test_name2(self):
-        """ """
+    def test_name(self):
+        """name atribute (string) """
         new = self.value()
         self.assertEqual(type(new.name), str)
