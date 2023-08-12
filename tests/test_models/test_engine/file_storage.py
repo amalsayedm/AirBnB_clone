d8a8d67fb6373b5e183125af62e2335cb2cbaa94
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-08-12 03:24:08 +0200

    Update file_storage.py

--- tests/test_models/test_engine/file_storage.py
+++ tests/test_models/test_engine/file_storage.py
@@ -8,8 +8,7 @@ import os
 
 class test_fileStorage(unittest.TestCase):
     """ Class to test the file storage method """
-    
-    
+
     def setUp(self):
         """ Set up test environment """
         del_list = []
@@ -18,7 +17,6 @@ class test_fileStorage(unittest.TestCase):
         for key in del_list:
             del storage._FileStorage__objects[key]
 
-    
     def tearDown(self):
         """ Remove storage file at end of tests """
         try:
