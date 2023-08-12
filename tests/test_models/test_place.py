9cc181533b159906f8ee4f1daa30f6cfeac0592a
janviave tharwat <91113365+JanviaveTharwat@users.noreply.github.com>
2023-08-12 02:41:10 +0200

    testing

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -11,8 +11,8 @@ class test_Place(test_basemodel):
     def __init__(self, *args, **kwargs):
         """ """
         super().__init__(*args, **kwargs)
-        self.name = "User"
-        self.value = User
+        self.name = "Place"
+        self.value = Place
 
 
     """atributes tests"""
